06daf36713609b2bd0cea210bd2961632f24a306
Aleks753 <47895824+Aleks753@users.noreply.github.com>
2021-04-16 18:47:23 +0300

    Update and rename bomba.py to Karen.py

---
+++ Karen.py
@@ -0,0 +1,31 @@
+from telethon import events
+import asyncio
+import os
+import sys
+
+
+@borg.on(events.NewMessage(pattern=r"\.karen", outgoing=True))
+async def _(event):
+    if event.fwd_from:
+        return
+       
+ 
+    await event.edit("▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n")
+    await asyncio.sleep(0.5)
+    await event.edit("🇰🇿🇰🇿🇰🇿🇰🇿 \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n")
+    await asyncio.sleep(0.5)
+    await event.edit("▪️▪️▪️▪️ \n🇰🇿🇰🇿🇰🇿🇰🇿 \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n")
+    await asyncio.sleep(0.5)
+    await event.edit("▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n🇰🇿🇰🇿🇰🇿🇰🇿 \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n")
+    await asyncio.sleep(0.5)
+    await event.edit("▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n🇰🇿🇰🇿🇰🇿🇰🇿 \n▪️▪️▪️▪️ \n")
+    await asyncio.sleep(0.5)
+    await event.edit("▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n🇰🇿🇰🇿🇰🇿🇰🇿 \n")
+    await asyncio.sleep(1)
+    await event.edit("▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n⚠️⚠️⚠️⚠️ \n")
+    await asyncio.sleep(0.5)
+    await event.edit("▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n‼️‼️‼️‼️ \n‼️‼️‼️‼️ \n")
+    await asyncio.sleep(0.5)
+    await event.edit("▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n▪️▪️▪️▪️ \n😮😮😮😮 \n")
+    await asyncio.sleep(0.5)
+    await event.edit("⚡⚡ АРМЯНИН КАЗАХСКОГО ПРОИСХОЖДЕНИЯ НАДРУГАЛСЯ НАД ГРАЖДАНИНОМ АЗЕРБАЙДЖАНА⚡⚡")
